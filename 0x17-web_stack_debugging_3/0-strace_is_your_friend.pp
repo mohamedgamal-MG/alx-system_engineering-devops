f7ec51af86cff0a8ca1ac93ff5a34b2aaebc18ac
mohamedgamal-MG <mohamedgamal.jemmy.zero@gmail.com>
2023-10-11 22:50:13 +0000

    up

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,16 @@
-# automated puppet fix (to find out why Apache is returning a 500 error)
-
-exec { 'Fix wordpress site':
-  command  => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
-  provider => shell,
+# Fix a file
+file { '/var/www/html/wp-settings.php':
+  ensure  => file,
+  source  => 'puppet:///modules/my_module/wp-settings.php', # You may need to define the template file in your module
+  owner   => 'www-data',  # Replace with the correct owner
+  group   => 'www-data',  # Replace with the correct group
+  mode    => '0644',
+  notify  => Exec['reload_apache'],  # Reload Apache after the modification
 }
+
+exec { 'reload_apache':
+  command     => 'systemctl reload apache2',  # You may need to modify the command according to your Ubuntu version
+  path        => '/bin:/sbin:/usr/bin:/usr/sbin',
+  refreshonly => true,
+}
+
